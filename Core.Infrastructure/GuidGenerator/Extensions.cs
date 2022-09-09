1b5634112d315251dd4335da884dcfd79b2e38a2
Alirexaa <Alireza.Baloochi1380@gmail.com>
2022-09-09 12:25:26 +0430

    added Sequential Guid generator

---
+++ Core.Infrastructure/GuidGenerator/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using Core.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Infrastructure.GuidGenerator
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddSequentialGuidGenerator(this IServiceCollection services)
+        {
+            services.AddSequentialGuidGenerator(SequentialGuidType.SequentialAtEnd);
+            return services;
+        }
+        public static IServiceCollection AddSequentialGuidGenerator(this IServiceCollection services,SequentialGuidType sequentialGuidType)
+        {
+            services.Configure<SequentialGuidGeneratorOptions>(options =>
+            {
+                options.DefaultSequentialGuidType = sequentialGuidType;
+            });
+            services.AddTransient<IGuidGenerator, ApplicationSequentialGuidGenerator>();
+            return services;
+        }
+    }
+}
---
+++ Core.Infrastructure/GuidGenerator/SequentialGuidGeneratorOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Infrastructure.GuidGenerator
+{
+    public class SequentialGuidGeneratorOptions
+    {
+
+        public SequentialGuidType? DefaultSequentialGuidType { get; set; }
+
+        public SequentialGuidType GetDefaultSequentialGuidType()
+        {
+            return DefaultSequentialGuidType ??
+                   SequentialGuidType.SequentialAtEnd;
+        }
+    }
+}
